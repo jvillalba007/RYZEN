b3ee445d2c7e2a320ee1083633982b711309c209
Javier Villalba <jvillalba@yandex.com>
2019-04-11 13:43:11 -0300

    Libreria Shared Sockets

---
+++ shared/shared/socket.c
@@ -0,0 +1,287 @@
+#include "socket.h"
+
+
+static void close_conection(void *conexion);
+static bool close_conection_condition(void *conexion);
+
+int msg_await(int socket, t_msg* msg){
+	if(socket == -1) return -1;
+
+	t_header* header = malloc(sizeof(t_header));
+	int result_recv = recv(socket, header, sizeof(t_header), MSG_WAITALL);
+	if(result_recv == -1){
+		free(header);
+		return -1;
+	}
+
+	void* payload = malloc(header->payload_size);
+	result_recv = recv(socket, payload, header->payload_size, MSG_WAITALL);
+	if(result_recv < 1){
+		free(header);
+		free(payload);
+		return -1;
+	}
+
+	msg->header = header;
+	msg->payload = payload;
+	return result_recv;
+}
+
+void msg_free(t_msg** msg){
+	if(msg != NULL && (*msg) != NULL){
+		if((*msg)->header != NULL){
+			if((*msg)->payload != NULL && (*msg)->header->payload_size > 0)
+				free((*msg)->payload);
+			free((*msg)->header);
+		}
+		free(*msg);
+	}
+}
+
+
+int socket_create_listener(char* ip, char* port){
+	if(port == NULL) return -1;
+
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = AF_UNSPEC;    // No importa si uso IPv4 o IPv6
+	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
+	hints.ai_socktype = SOCK_STREAM;  // Indica que usaremos el protocolo TCP
+
+	getaddrinfo(ip, port, &hints, &server_info);  // Si IP es NULL, usa el localhost
+
+	int server_socket = socket(server_info->ai_family, server_info->ai_socktype,server_info->ai_protocol);
+
+	int activado = 1;
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+	if(server_socket == -1 || bind(server_socket, server_info->ai_addr, server_info->ai_addrlen) == -1){
+		freeaddrinfo(server_info);
+		return -1;
+	}
+
+	freeaddrinfo(server_info);
+
+	if(listen(server_socket, BACKLOG) == -1) return -1;
+	return server_socket;
+}
+
+
+int socket_connect_to_server(char* ip, char* port) {
+	if(ip == NULL || port == NULL) return -1;
+
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = AF_UNSPEC;    // No importa si uso IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM;  // Indica que usaremos el protocolo TCP
+
+	getaddrinfo(ip, port, &hints, &server_info);
+
+	int server_socket = socket(server_info->ai_family, server_info->ai_socktype,server_info->ai_protocol);
+
+	int retorno = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
+
+	freeaddrinfo(server_info);
+
+	return (retorno < 0 || server_socket == -1) ? -1 : server_socket;
+}
+
+int socket_aceptar_conexion(int socketServidor) {
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
+
+	int socketCliente = accept(socketServidor, (struct sockaddr *) &addr, &addrlen);
+	if (socketCliente < 0) {
+
+		perror("Error al aceptar cliente");
+		return -1;
+	}
+
+	return socketCliente;
+}
+
+char* get_ip_socket(int fd){
+	struct sockaddr_in addr;
+	socklen_t addr_size = sizeof(struct sockaddr_in);
+	int res = getpeername(fd, (struct sockaddr *)&addr, &addr_size);
+	if(res == -1){
+		return NULL;
+	}
+	char ipNodo[20];
+	strcpy(ipNodo, inet_ntoa(addr.sin_addr));
+	return strdup(ipNodo);
+}
+
+void socket_start_listening_select(int socketListener, int (*manejadorDeEvento)(int, t_msg*), ...){
+
+	typedef struct fd_parametro{
+		e_emisor emisor;
+		e_tipo_msg tipo_msg;
+		int fd;
+	}t_fd_parametro;
+
+	t_list* fds_por_parametro = list_create();
+
+	int _get_max_fd(){
+
+		void* _fd_mayor(void* max_fd_hasta_ahora, void* fd_param){
+			return (void*) max((int) max_fd_hasta_ahora, ((t_fd_parametro*) fd_param)->fd);
+		}
+
+		if(list_is_empty(fds_por_parametro)){
+			return socketListener;
+		}
+
+		int max_fd_parametro = (int) list_fold(fds_por_parametro, 0, _fd_mayor);
+		return max(socketListener, max_fd_parametro);
+	}
+
+	//Por si me mandan un socket con problemas
+	if(socketListener == -1) return;
+
+ 	va_list arguments;
+	va_start(arguments, manejadorDeEvento);
+
+	int i, cant_fd_por_parametro = va_arg(arguments, int);
+
+	for(i = 0; i<cant_fd_por_parametro; i++){
+		t_fd_parametro* fd = malloc(sizeof(t_fd_parametro));
+		fd->emisor = va_arg(arguments, e_emisor);
+		fd->tipo_msg = va_arg(arguments, e_tipo_msg);
+		fd->fd = va_arg(arguments, int);
+		list_add(fds_por_parametro, fd);
+	}
+
+	t_list* conexiones = list_create();
+
+	int activity, fdMax = _get_max_fd(); // El mayor entre el socket de escucha y todos los FD pasados por parametro
+	fd_set readfds;
+
+	while(1){
+		//Vacio el set e incluyo al socket de escucha
+		FD_ZERO(&readfds);
+		FD_SET(socketListener, &readfds);
+
+		//Agrego a todas las conexiones
+		for(int i = 0; i < list_size(conexiones); i++){
+			FD_SET( ((t_conexion*) list_get(conexiones, i))->socket , &readfds);
+		}
+
+		//Agrego a todos los FDs pasados por parametro
+		for(int i = 0; i < list_size(fds_por_parametro); i++){
+			FD_SET( ((t_fd_parametro*) list_get(fds_por_parametro, i))->fd , &readfds);
+		}
+
+		//Esperamos que ocurra algo con alguna de las conexiones (inclusive con el socket de escucha)
+		activity = select( fdMax + 1, &readfds, NULL, NULL, NULL);
+
+		if (activity < 0) {
+			//Ocurrio un error
+			continue;
+		}
+
+		//Checkeamos si ocurrio algo con el socket de escucha
+		if(FD_ISSET(socketListener, &readfds)){
+			t_conexion* conexion = malloc(sizeof(t_conexion));
+			conexion->addr = malloc(sizeof(struct sockaddr));
+
+			//Calcula el espacio de la direccion de la nueva conexion
+			socklen_t addrSize = sizeof(conexion->addr);
+
+			//Acepto esta nueva conexion
+			int nuevoSocket = accept(socketListener, conexion->addr, &addrSize);
+			conexion->socket = nuevoSocket;
+
+			//Pregunto si salio toodo bien
+			if(nuevoSocket == -1) close_conection(conexion);
+
+			//Añado la conexion a la lista
+			list_add(conexiones, conexion);
+
+			//Creamos un nuevo mensaje para avisar de la nueva conexion
+			t_msg *msg = malloc(sizeof(t_msg));
+			msg->header = malloc(sizeof(t_header));
+			msg->header->emisor = DESCONOCIDO;
+			msg->header->tipo_mensaje = CONEXION;
+			msg->header->payload_size = 0;
+			msg->payload = NULL;
+
+			//Llamo a la funcion encargada de manejar las nuevas conexiones
+			manejadorDeEvento(conexion->socket, msg);
+			msg_free(&msg);
+
+			//Seteo el nuevo fd maximo
+			fdMax = nuevoSocket > fdMax ? nuevoSocket:fdMax;
+		}
+
+		//Recorremos preguntando por cada conexion si ocurrio algun evento
+		for(int i = 0; i < list_size(conexiones); i++){
+
+			if(FD_ISSET( ((t_conexion*) list_get(conexiones, i))->socket , &readfds )){ //Ocurrio algo con este socket
+				//Recibo el mensaje
+				t_msg* msg = malloc(sizeof(t_msg));
+				msg->header = NULL;
+				msg->payload = NULL;
+				if(msg_await( ((t_conexion*) list_get(conexiones, i))->socket, msg) == -1){
+					msg->header = malloc(sizeof(t_header));
+					msg->header->emisor = ((t_conexion*) list_get(conexiones, i))->conectado;
+					msg->header->tipo_mensaje = DESCONEXION;
+					msg->header->payload_size = 0;
+					msg->payload = NULL;
+					manejadorDeEvento(((t_conexion*) list_get(conexiones, i))->socket, msg);
+					socket_a_destruir = ((t_conexion*) list_get(conexiones, i))->socket;
+					list_remove_and_destroy_by_condition(conexiones, close_conection_condition, close_conection);
+				}else{
+					((t_conexion*) list_get(conexiones, i))->conectado = msg->header->emisor;
+					if( manejadorDeEvento(((t_conexion*) list_get(conexiones, i))->socket, msg) == -1){
+						//Si es -1 significa que por alguna razon quiere que cierre la conexion
+						socket_a_destruir = ((t_conexion*) list_get(conexiones, i))->socket;
+						list_remove_and_destroy_by_condition(conexiones, close_conection_condition, close_conection);
+					}
+				}
+				msg_free(&msg);
+			}
+		}
+
+		// Recorremos preguntando por cada FD pasado por parametro, si ocurrio algun evento
+		for(i = 0; i<list_size(fds_por_parametro); i++){
+			if(FD_ISSET( ((t_fd_parametro*) list_get(fds_por_parametro, i))->fd , &readfds )){ // Ocurrio algo con este FD
+				t_msg* msg = malloc(sizeof(t_msg));
+				msg->header = malloc(sizeof(t_header));
+				msg->header->emisor = ((t_fd_parametro*) list_get(fds_por_parametro, i))->emisor;
+				msg->header->tipo_mensaje = ((t_fd_parametro*) list_get(fds_por_parametro, i))->tipo_msg;
+				msg->header->payload_size = 0;
+				msg->payload = NULL;
+				manejadorDeEvento(((t_fd_parametro*) list_get(fds_por_parametro, i))->fd, msg);
+				msg_free(&msg);
+			}
+		}
+	}
+}
+
+static void close_conection(void *conexion){
+	if(conexion != NULL){
+		if(((t_conexion*)conexion)->socket != -1)
+			close(((t_conexion*)conexion)->socket);
+		if(((t_conexion*)conexion)->addr != NULL)
+			free(((t_conexion*)conexion)->addr);
+		free(conexion);
+	}
+	return;
+}
+
+static bool close_conection_condition(void *conexion){
+	if(conexion != NULL){
+		if ((((t_conexion*)conexion)->socket) == socket_a_destruir){
+			return true;
+		}
+	}
+	return false;
+}
+
---
+++ shared/shared/socket.h
@@ -0,0 +1,97 @@
+#ifndef SHARED_SOCKET_H_
+#define SHARED_SOCKET_H_
+	#include <stdio.h>
+	#include <stdbool.h>
+	#include <string.h>
+	#include <stdlib.h>
+	#include <stdarg.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
+	#include <unistd.h>
+	#include <commons/collections/list.h>
+
+	typedef enum emisor{DESCONOCIDO, KERNEL,MEMORIA,LFS} e_emisor;
+	typedef enum {CONEXION,DESCONEXION} e_tipo_msg;
+
+	typedef struct {
+		e_emisor emisor;
+		e_tipo_msg tipo_mensaje;
+		int payload_size;
+	} __attribute__((packed)) t_header;
+
+	typedef struct {
+		t_header* header;
+		void* payload;
+	} t_msg;
+	
+	typedef struct {
+		int socket;
+		e_emisor conectado;
+		__SOCKADDR_ARG addr;
+	} t_conexion;
+
+	#define BACKLOG 100
+
+	#define max(a,b) \
+	({ __typeof__ (a) _a = (a); \
+	__typeof__ (b) _b = (b); \
+	_a > _b ? _a : _b; })
+
+	int socket_a_destruir;
+
+	/**
+	* @NAME: msg_await
+	* @DESC: Espera hasta recibir un mensaje. El socket ya debe estar escuchando
+	*/
+	int msg_await(int socket, t_msg* mensaje);
+
+	/**
+	* @NAME: msg_free
+	* @DESC: Libera la memoria de un mensaje
+	*/
+	void msg_free(t_msg** msg);
+
+	/**
+	* @NAME: socket_create_listener
+	* @DESC: Creo un socket de escucha y lo devuelvo, o -1 se hubo error. Me pongo a escuchar.
+	* @PARAMS:
+	* 		ip   - ip del server. Si es NULL, usa el localhost: 127.0.0.1
+	* 		port - puerto en el que escuchar
+	*/
+	int socket_create_listener(char* ip, char* port);
+
+	/**
+	* @NAME: socket_connect_to_server
+	* @DESC: Me conecto al server, y devuelvo el socket, o -1 si hubo error
+	*/
+	int socket_connect_to_server(char* ip, char* port);
+
+	/**
+	* @NAME: socket_aceptar_conexion
+	* @DESC: Acepta una nueva conexión y devuelve el nuevo socket conectado
+	*/
+	int socket_aceptar_conexion(int socket_servidor);
+
+	/**
+	* @NAME: socket_get_ip
+	* @DESC: Devuelve la IP de un socket
+	*/
+	char* socket_get_ip(int fd);
+
+	/**
+	* @NAME: socket_start_listening_select
+	* @DESC: Gestiona eventos en un socket con la funcion que es parametro + en los FD adicionales
+	* @PARAMS:
+	* 	socketListener -
+	* 	manejadorDeEvento - funcion a llamar en el caso de un evento con un descriptor de archivo
+	* 	... - FD adicionales a multiplexar. Al principio, se debe indicar la cantidad de FDs, y para c/u su emisor y el tipo de msj.
+	* 	      Ejemplos:
+	* 		      1, SAFA, INOTIFY, fd_inotify
+	* 		      2, SAFA, INOTIFY, fd_inotify1, SAFA, INOTIFY, fd_inotify2
+	*/
+	void socket_start_listening_select(int socketListener, int (*manejadorDeEvento)(int, t_msg*), ...);
+
+
+#endif /* SHARED_SOCKET_H_ */
