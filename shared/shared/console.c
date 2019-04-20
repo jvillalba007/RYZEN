088e2459b6a3aa26fb9298b37d4d59740d6c9428
Javier Villalba <jvillalba@yandex.com>
2019-04-20 19:35:21 -0300

    Readline en Console(Shared)

--- shared/shared/console.c
+++ shared/shared/console.c
@@ -1,25 +1,6 @@
 #include "console.h"
 
-size_t bufsize = 32;
+char* console() {
 
-
-size_t console(char *buffer) {
-
-
-    size_t characters;
-
-
-    if( buffer == NULL)
-    {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-
-    printf("> ");
-    characters = getline(buffer,&bufsize,stdin);
-    //printf("%zu characters were read.\n",characters);
-
-    return characters;
-
+	return readline("Lissandra>> ");
 }
-
--- shared/shared/console.h
+++ shared/shared/console.h
@@ -3,30 +3,15 @@
 
 	#include <stdio.h>
 	#include <stdlib.h>
-
+	#include <readline/readline.h>
 
 	/**
 	* @NAME: console
-	* @DESC: Recibe un buffer alocado con los chars necesarios para almacenar el mensaje. Lee por console el mensaje y lo mete
-	* en la variable buffer. Retorna el tipo de dato size_t que dice la cantidad de caracteres que recibió, contando \0.
-	* No olvidarse de freerear el buffer una vez procesado.
-	* Ejemplo:
-	int main(void){
-		char *buffer;
-		size_t n_characters;
-
-		buffer = (char *)malloc(bufsize * sizeof(char));
-
-		n_characters = console(&buffer);
-
-		printf("You typed: '%s'\n", buffer);
-
-		free(buffer);
-	}
+	* @DESC: Retorna puntero string de lo que lee readline por consola
 
 	*/
 
-	size_t console(char *buffer);
+	char* console();
 
 
 #endif /* SHARED_SOCKET_H_ */
