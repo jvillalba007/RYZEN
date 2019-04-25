b9aa85f95c464acbbf779a942120141deb0de755
Javier Villalba <jvillalba@yandex.com>
2019-04-25 16:14:58 -0300

    HiloConsolaMemoria

--- MEMORIA/src/memoria.h
+++ MEMORIA/src/memoria.h
@@ -8,7 +8,9 @@
 #ifndef MEMORIA_H_
 	#define MEMORIA_H_
 	#include "config/config.h"
+	#include "consola.h"
 	#include <shared/socket.h>
+	#include <pthread.h>
 
 int socketServidor;
 int socketClienteLfs;
@@ -17,6 +19,5 @@ void inicializar(); /* inicializa todas las estructuras funcionales para la memo
 void crear_servidor(); /* crea servidor de la memoria donde recibira las request de kernel y de otras meomrias */
 void crear_cliente_lfs(); /* crea cliente para poder enviar las request hacia el LFS */
 void ejecutar_gossiping(); /* ejecuta proceso de gossiping cada x tiempo */
-void consola(); /* consola de MEMORIA */
 
 #endif /* MEMORIA_H_ */
