2cb56c9893bf48fc5b9e1dc945cfb05f874b0c3e
Axel <axel.furlan95@gmail.com>
2019-04-28 21:50:24 -0300

    procesar comandos

--- LFS/src/LFS.c
+++ LFS/src/LFS.c
@@ -36,6 +36,78 @@ void crear_servidor(){
 
 }
 
+
+
+void consola_procesar_comando(char* linea)
+{
+	char** parametros = string_split(linea, " ");
+	int cantParametros = split_cant_elem(parametros);
+
+	if(string_equals_ignore_case(parametros[0],"INSERT")){
+		if (cantParametros >= 4 && cantParametros < 6) {
+			char** parametros_aux = string_split(linea, "\""); //spliteo la linea con comillas
+			char** parametros_estaticos  = string_split(parametros_aux[0], " "); //obtiene INSERT,[TABLA],[KEY]
+			string_iterate_lines(parametros_estaticos,puts);
+			puts(parametros_aux[1]); //parametros_aux[1] esta el "VALUE"
+			split_liberar(parametros_estaticos);
+			split_liberar(parametros_aux);
+		}else{
+			perror("API Error: 3 o 4 argumentos son requeridos");
+		}
+	}
+
+	else if(string_equals_ignore_case(parametros[0],"SELECT")){
+		if (cantParametros == 3) {
+			string_iterate_lines(parametros,puts);
+		} else {
+			perror("API Error: 2 argumentos son requeridos.");
+		}
+
+	}
+
+	else if(string_equals_ignore_case(parametros[0],"CREATE")){
+		if (cantParametros == 5) {
+			string_iterate_lines(parametros,puts);
+		} else {
+			perror("API Error: 4 argumentos son requeridos.");
+		}
+
+	}
+
+	else if(string_equals_ignore_case(parametros[0],"DESCRIBE")){
+		if (cantParametros >= 1 && cantParametros < 3) {
+			string_iterate_lines(parametros,puts);
+		} else {
+			perror("API Error: ninguno o 1 argumento es requerido.");
+		}
+
+	}
+
+	else if(string_equals_ignore_case(parametros[0],"DROP")){
+		if (cantParametros == 2) {
+			string_iterate_lines(parametros,puts);
+		} else {
+			perror("API Error: 1 argumento es requerido.");
+		}
+
+	}
+
+	/* Comando clear (limpiar pantalla consola) */
+	else if(cantParametros == 1 && string_equals_ignore_case(parametros[0],"CLEAR")){
+		system("clear");
+	}
+
+	/* Error al ingresar comando */
+	else{
+		printf("No se pudo reconocer el comando\n");
+
+	}
+
+	split_liberar(parametros);
+}
+
+
+
 void console_process() {
 
 
@@ -52,6 +124,10 @@ void console_process() {
 				EXIT_PROGRAM = true;
 				shutdown(socketServidor,SHUT_RDWR);
 			}
+		else {
+			consola_procesar_comando(buffer);
+		}
+
 		free(buffer);
 
 	}
@@ -77,7 +153,7 @@ int main(void) {
 	pthread_attr_init(&attr_server);
 
 	pthread_t tid_server;
-	pthread_create(&tid_server, &attr_server, crear_servidor, NULL);
+	pthread_create(&tid_server, &attr_server, (void*)crear_servidor, NULL);
 	//pthread_detach(tid_server);
 
 	// INICIAR CONSOLA
@@ -85,7 +161,7 @@ int main(void) {
 	pthread_attr_init(&attr_consola);
 
 	pthread_t tid_consola;
-	pthread_create(&tid_consola, &attr_consola, console_process, NULL);
+	pthread_create(&tid_consola, &attr_consola, (void*)console_process, NULL);
 
 	//Esperar a que el hilo termine
 	pthread_join(tid_consola, NULL);
--- LFS/src/LFS.h
+++ LFS/src/LFS.h
@@ -3,12 +3,14 @@
 	#include <stdio.h>
 	#include <string.h>
 	#include <stdlib.h>
-	#include<commons/log.h>
+	#include <commons/log.h>
 	#include <commons/config.h>
+	#include <commons/string.h>
 	#include <pthread.h>
 	#include <shared/socket.h>
 	#include <shared/console.h>
 
+	#include "shared/utils.h"
 	#include "config/config_LFS.h"
 
 #endif /* LFS_H */
@@ -19,3 +21,4 @@ int socketServidor;
 void crear_servidor(void);
 void console_process(void);
 void liberar_memoria(void);
+void consola_procesar_comando(char*);
