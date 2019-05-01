ad0c2311a1d0908c3124de9130b0eda99f9a8597
Axel <axel.furlan95@gmail.com>
2019-05-01 13:12:35 -0300

    utils comments

--- shared/shared/utils.h
+++ shared/shared/utils.h
@@ -5,9 +5,21 @@
 	#include <stdlib.h>
 	#include <string.h>
 
+	// Libera memoria de un array de chars recorriendo 1 por 1
 	void split_liberar(char **);
+	// Cuenta la cantidad de strings que hay en un array
 	int split_cant_elem(char **);
+	/* Recibe el string a procesar, el string-patrón con el que
+	 * debería empezar y otro con el que debería terminar.
+	 * Devuelve el string que extrajo en el medio.
+	 * ADVERTENCIA: LIBERAR ESE STRING DESPUES DE USAR.
+	*/
 	char* string_extract_substring(char*, char*, char*);
+	/* Recibe 2 strings: el primero es la linea a procesar y el segundo
+	 * es lo que hay que quitar de ese primer string.
+	 * Ejemplo: remove_substring("hola como estas", "como") => "hola  estas"
+	 * Liberar memoria de los strings si es necesario.
+	 */
 	void remove_substring (char *, char*);
 
 #endif /* SHARED_SOCKET_H_ */
