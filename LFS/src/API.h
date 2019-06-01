8be61bf3c8d5a54d088a53ce61528e6adfb2f631
Axel <axel.furlan95@gmail.com>
2019-06-01 18:50:48 -0300

    advancing with create

--- LFS/src/API.h
+++ LFS/src/API.h
@@ -5,6 +5,8 @@
 		#include <string.h>
 		#include <stdlib.h>
 		#include <unistd.h>
+		#include <sys/types.h>
+		#include <sys/stat.h>
 		#include <commons/string.h>
 		#include <errno.h>
 		#include <time.h>
