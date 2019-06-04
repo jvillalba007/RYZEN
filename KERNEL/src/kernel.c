261ad24843e98616ebb4b381b145ef6297b94dd3
Pablo <saldiviape@gmail.com>
2019-06-04 02:49:52 -0300

    Estructuras creadas para el uso de listas

--- KERNEL/src/kernel.c
+++ KERNEL/src/kernel.c
@@ -40,54 +40,5 @@ int main() {
 }
 
 
-
-/* otra consola...
-void consola(size_t TAMANIO_BUFFER) {
-	//	Un bucle infinito porque la consola permanecerá abierta (casi) siempre.
-	while (1) {
-		char* buffer_linea_leida;
-		size_t cantidad_caracteres;
-		buffer_linea_leida = (char*) malloc(TAMANIO_BUFFER * 2 * sizeof(char));
-		cantidad_caracteres = console(&buffer_linea_leida);
-		log_info(logger, buffer_linea_leida);
-		//	A continuación se empieza a parsear lo escrito por consola...
-		//	Si se lee el comando EXIT, se saldrá del bucle.
-		if (strcmp(buffer_linea_leida, "EXIT\n") == 0) {
-			puts("Cerrando consola en 3...");
-			sleep(1);
-			puts("2...");
-			sleep(1);
-			puts("1...");
-			sleep(1);
-			break;
-		}
-		
-		//	Si se lee el comando DESCRIBE (sin recibir parámetros)...
-		if (strcmp(buffer_linea_leida, "DESCRIBE\n") == 0) {
-			//	--> MEMORIA --> "obtener la metadata de todas las tablas que tenga el FS" --> LFS
-			puts("Pusiste un DESCRIBE\n");
-		}
-		
-		//	Si se lee el comando JOURNAL...
-		if (strcmp(buffer_linea_leida, "JOURNAL\n") == 0) {
-			//	"enviar JOURNAL a cada memoria asociada" 
-			puts("Pusiste un JOURNAL\n");
-		}
-		
-		//	Si se lee el comando METRICS...
-		if (strcmp(buffer_linea_leida, "METRICS\n") == 0) {
-			//	llamar a función mostrarMetricasActuales()
-			puts("Pusiste un METRICS\n");
-		}
-		
-		//	Acá viene la posta de la milanesa del parser...
-		//	Ver funciones de las commons para laburar con strings: string_starts_with y string_split
-		if (string_starts_with(buffer_linea_leida, "SELECT")) {
-			puts("Pusiste un SELECT\n");
-		}
-		//	Acá se termina de parsear.
-		free(buffer_linea_leida);
-	}
-	pthread_exit(0);
-}
-*/
+//	Lineamientos del Kernel
+//	https://docs.google.com/document/d/1nus6JWLj3mjzUATejoatIK_qFewLztolEKRa0jXsa0I
