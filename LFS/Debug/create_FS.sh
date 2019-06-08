600b2b0a2d16ccc98ac0d79b5f8f7cc0a349bf0b
Axel <axel.furlan95@gmail.com>
2019-06-08 16:39:17 -0300

    create FS shell script

---
+++ LFS/Debug/create_FS.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# positional arguments
+folder="$1"
+blocks="$2"
+
+create_blocks(){
+	cd bloques
+	for i in `seq 1 $blocks`
+	do
+	    touch "$i".bin	
+	    echo "$i".bin created
+	done
+	cd ..
+}
+
+create_metadata(){
+	cd Metadata
+	echo "BLOCK_SIZE=64
+BLOCKS=$blocks
+MAGIC_NUMBER=LISSANDRA" > Metadata.bin
+	echo Metadata.bin created
+	cd ..
+}
+
+create_bitmap(){
+	cd Metadata
+	printf '0%.0s' $(seq 1 $blocks) > Bitmap.bin
+	cd ..
+}
+
+# Main
+echo $blocks blocks
+echo folder is $folder
+
+mkdir $folder
+cd $folder
+
+mkdir tables
+mkdir bloques
+mkdir Metadata
+
+create_blocks
+create_metadata
+create_bitmap
