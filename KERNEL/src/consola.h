b4afc53c1bc4622177ab6e56096d322052225924
Fran <fran.ferron07@gmail.com>
2019-07-18 01:45:03 -0300

    KERNEL

---
+++ KERNEL/src/consola.h
@@ -0,0 +1,29 @@
+#ifndef CONSOLA_H
+#define CONSOLA_H
+
+#include <shared/console.h>
+#include "shared/utils.h"
+#include "shared/socket.h"
+#include <commons/string.h>
+#include <pthread.h>
+#include "commons/commons.h"
+#include <time.h>
+
+
+void consola ();
+void procesar_comando (char*);
+void crear_pcb (char* string_codigo, t_tipo_request tipo);
+
+t_memoria_del_pool* obtener_memoria (int);
+void enviar_journal_lista_memorias (t_list* ); //recibe una lista y envia un journal a las memorias que esten activas de esa lista
+bool es_comando_conocido (char**);
+bool es_correcta_cantidad_parametros (char*, int);
+bool es_comando_planificable (char*);
+bool es_string (char*, char*);
+
+void notificar_error_comando_incorrecto (void);
+void notificar_error_comando_cantidad_parametros (void);
+void notificar_error_tipo_consistencia (void);
+
+void ejecutar_metricas(t_memoria_del_pool* memoria);
+#endif
--- KERNEL/src/kernel.h
+++ KERNEL/src/kernel.h
@@ -2,12 +2,61 @@
 #define KERNEL_H
 
 #include <pthread.h>
-#include "config/configKernel.h"
+#include "commons/commons.h"
+#include "consola.h"
 #include <shared/socket.h>
 #include <shared/console.h>
+#include <shared/protocolo.h>
+#include "shared/utils.h"
 
-/*  Definiciones de variables y funciones
-size_t TAMANIO_BUFFER = 32;
-//void consola(size_t);*/
+
+pthread_t tid_estadisticas;
+pthread_t tid_gossiping;
+pthread_t tid_describe;
+pthread_t tid_inotify;
+
+void conectar_memoria();
+void inicializar_kernel(); //inicializa las listas que se utilizaran en kernel
+void ejecutar_procesador(); //ejecuta en un hilo los pcb
+
+t_PCB* obtener_pcb_ejecutar(); // obtiene un pcb a ejecutar de la listo de ready.
+int ejecutar_linea( char *linea ); //recibe una linea y la envia a ejecutar a alguna memoria
+t_tabla_consistencia *obtener_tabla( char* n_tabla); //devuelve la tabla a partir del nombre
+t_memoria_del_pool *obtener_memoria_criterio( t_tabla_consistencia* tabla, char* linea); //a partir de una tabla me indica que memoria va a realizar la request segun criterio
+t_memoria_del_pool *obtener_memoria_SC();
+t_memoria_del_pool *obtener_memoria_EC();
+t_memoria_del_pool *obtener_memoria_SHC(char* linea);
+int ejecutar_linea_memoria( t_memoria_del_pool* memoria , char* linea ); //ejecuta en memoria la linea
+
+void crear_procesadores();
+void ejecutar_describe();
+char* obtener_linea(FILE* archivo);
+void apuntar_archivo(FILE* archivo, int pc);
+
+void finalizar_pcb(t_PCB* pcb);//quita pcb de ejecucion y lo pasa a finalizados
+void parar_por_quantum(t_PCB* pcb);//quita pcb de ejecucion y lo pasa a listos
+void reinicio_estadisticas();
+void reiniciar_memorias(t_memoria_del_pool* memoria);
+
+int enviar_insert(linea_insert linea, void* socket);
+int enviar_select(linea_select linea, void* socket);
+int enviar_create(linea_create linea, void* socket);
+int enviar_describe_general(void* socket);
+int enviar_describe_especial(void* socket, char* tabla);
+int enviar_drop(void* socket, char* tabla);
+
+void hilo_gossiping(); //Ejecuta cada x tiempo el gossiping con alguna memoria
+void gossiping( t_memoria_del_pool *memoria );
+void agregar_memoria_gossip( pmemoria *memoria ); //verifica si la memoria recibida existe o no en la tabla para agregarla o desecharla
+
+void agregar_tabla_describe( linea_create* tabla_describe ); //agrega tabla en el sistema si no existe
+
+void hilo_describe();
+int describe();
+
+void quitar_tabla_lista( char* tabla );//a partir de un nombre de tabla la busca en la metadata y la borra si existe
+void desactivar_memoria(t_memoria_del_pool *memoria); //desactiva la memoria por una desconexion. la quita de los criterios donde esta asociada.
+
+void inotify_config();
 
 #endif
